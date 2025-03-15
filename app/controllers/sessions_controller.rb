cd3e7d4952ff8d35018e3befa1ead76ae0fc9d62
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-03-15 04:37:38 +0000

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,14 +11,14 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     if @user != nil #or if @user.present instead of 'is not nil'
       # 3. if they know their password -> login is successful
-      if BCrypt::Password.new(@user["password"]) == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"] #this checks the password
 
-        # --- Begin user session
+        # --- Begin user session. 
         cookies["name"] = "Cookie Monster"
         cookies["saying"] = "i like cookies"
-        session["user_id"] = @user["id"] #securing user ID in the cookie jar
+        session["user_id"] = @user["id"] #securing user ID encrypted in the cookie jar. Creates an encrypted cookie! Won't be seen in the jar.
+        #Now the cookie takes the "name" and holds it over different pages, and can access. Can set expiration times, or when browser closes.
         
-
         flash["notice"] = "Welcome."
         redirect_to "/companies" 
       # 4. if the user doesn't exist or they don't know their password -> login fails
